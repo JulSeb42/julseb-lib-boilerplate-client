6df0655a8691efd080bafe21ca47bb769b7a5538
JulSeb42 <julien.sebag@me.com>
2024-11-01 07:59:33 +0100

    Clean

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -7,7 +7,6 @@ export const Homepage = () => {
     return (
         <Page title="Home">
             <Text tag="h1">Hello World!</Text>
-            <Text>Newest repo</Text>
         </Page>
     )
 }
